67d6706e01cc3766372c3ce22a4fc0e126cdbf22
AlexGarcia <afgarcia666@gmail.com>
2020-01-06 17:40:31 -0500

    updated css and added class name to hamburger button

--- client/src/components/Hamburger/Menu.js
+++ client/src/components/Hamburger/Menu.js
@@ -2,6 +2,6 @@ import React from 'react'
 import './Menu.css'
 
 
-const Menu = (props) => <button onClick={props.onClick}>{props.children}</button>
+const Menu = (props) => <button className="hamburger" onClick={props.onClick}>{props.children}</button>
 
 export default Menu
