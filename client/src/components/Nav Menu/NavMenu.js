ad7a8ecdd9f0d717e39578f43127369ddbe9f0bf
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-06 16:47:52 -0500

    fixed dot visibility

--- client/src/components/Nav Menu/NavMenu.js
+++ client/src/components/Nav Menu/NavMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 import './NavMenu.css'
 
 class NavMenu extends Component {
@@ -8,11 +9,26 @@ class NavMenu extends Component {
             <div className="nav-container">
                 <nav>
                     <ul>
-                        <li><h3>Home</h3><span className="dots">&#9679;</span></li>
-                        <li><h3>Our Gowns</h3><span className="dots">&#9679;</span></li>
-                        <li><h3>Visit Us</h3><span className="dots">&#9679;</span></li>
-                        <li><h3>Book An Appointment</h3><span className="dots">&#9679;</span></li>
-                        <li><h3>About Us</h3></li>
+                        <li>
+                                <h3>Home</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                         
+                                <h3>Our Gowns</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                            <h3>Visit Us</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                                <h3>Book An Appointment</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                           <h3>About Us</h3>
+                        </li>
                     </ul>
                 </nav>
             </div>
