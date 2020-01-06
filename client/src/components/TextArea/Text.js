f6a6e8b8c92e6fb17036ad63685072d1d618ede9
bukharov <dilmurod.bukharov@gmail.com>
2020-01-06 16:20:15 -0500

    created TextArea component

---
+++ client/src/components/TextArea/Text.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './Text.css'
+
+const Text = (props) => <p>{props.children}</p>
+
+export default Text
---
+++ client/src/components/TextArea/Text.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Text from './Text';
+
+storiesOf('Text', module)
+    .add('TextArea', () => <p>This is a text</p>)
