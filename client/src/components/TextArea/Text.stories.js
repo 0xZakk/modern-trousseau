7f296893f8ba28cbe952e4a9946e20c8a84d3dfe
bukharov <dilmurod.bukharov@gmail.com>
2020-01-06 17:16:37 -0500

    final thing

--- client/src/components/TextArea/Text.stories.js
+++ client/src/components/TextArea/Text.stories.js
@@ -4,4 +4,4 @@ import { storiesOf } from '@storybook/react';
 import Text from './Text';
 
 storiesOf('Text', module)
-    .add('TextArea', () => <p>This is a text</p>)
+    .add('TextArea', () => <div><p className="title">Is there anything else you want us to know?</p><div className="box"><p>Tell us about your dream dress! Do you have any Modern Trousseau favorites?</p></div></div>)
