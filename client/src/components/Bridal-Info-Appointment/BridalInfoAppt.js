34449edaed9c88d160a9d063a8b039201b75f216
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-07 09:44:05 -0500

    linked storybook

---
+++ client/src/components/Bridal-Info-Appointment/BridalInfoAppt.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BridalInfoAppt extends Component {
+    render() {
+        return (
+            <>
+            </>
+        )
+    }
+}
+
+export default BridalInfoAppt;
---
+++ client/src/components/Bridal-Info-Appointment/BridalInfoAppt.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import BridalInfoAppt from './BridalInfoAppt'
+
+storiesOf('BridalInfoAppt', module)
+    .add('BridalInfoAppt', () => <BridalInfoAppt />)
