d3c92d507e274553a2fe62cd75e60ccb37f46fcf
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-07 15:31:22 -0500

    fixed props statement so flag is now .is-visible

--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -1,44 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./NavMenu.css";
 
-const NavMenu = props =>
-  props.isVisible ? (
-    <div className="nav-container">
-      <nav>
-        <ul>
-          <li>
-            <Link to="#">
-              <h3 className="nav-title">Home</h3>
-              <span className="dots">&#9679;</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <h3 className="nav-title">Our Gowns</h3>
-              <span className="dots">&#9679;</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <h3 className="nav-title">Visit Us</h3>
-              <span className="dots">&#9679;</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <h3 className="nav-title">Book An Appointment</h3>
-              <span className="dots">&#9679;</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="#">
-              <h3 className="nav-title">About Us</h3>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  ) : null;
+const NavMenu = props => (
+  <div
+    className={props.isVisible ? "nav-container" : "nav container is-vibible"}
+  >
+    <nav>
+      <ul>
+        <li>
+          <Link to="#">
+            <h3 className="nav-title">Home</h3>
+            <span className="dots">&#9679;</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <h3 className="nav-title">Our Gowns</h3>
+            <span className="dots">&#9679;</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <h3 className="nav-title">Visit Us</h3>
+            <span className="dots">&#9679;</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <h3 className="nav-title">Book An Appointment</h3>
+            <span className="dots">&#9679;</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="#">
+            <h3 className="nav-title">About Us</h3>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
 
 export default NavMenu;
