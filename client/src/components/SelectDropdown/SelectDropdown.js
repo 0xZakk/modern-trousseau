ff8e640359c55f4ad7586e69513938c5a60e6029
Adam_Bates <Creizai@EMAIL.COM>
2020-01-07 09:59:48 -0500

    deleted and created new files properly named.  Filled with button skeleton for now

---
+++ client/src/components/SelectDropdown/SelectDropdown.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./SelectDropdown.css";
+
+const SelectDropdown = props => <button>{props.children}</button>;
+
+export default SelectDropdown;
---
+++ client/src/components/SelectDropdown/SelectDropdown.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import SelectDropdown from "./SelectDropdown";
+
+storiesOf("SelectDropdown", module).add("SelectDropdown", () => (
+  <SelectDropdown>This is a SelectDropdown</SelectDropdown>
+));
