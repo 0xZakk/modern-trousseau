7373d61ae8dbe44fc5d566fb020813d02bb9f45f
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-07 12:40:56 -0500

    added in storybook router to navmenu.stories.js

--- client/src/components/NavMenu/NavMenu.stories.js
+++ client/src/components/NavMenu/NavMenu.stories.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import StoryRouter from 'storybook-react-router';
 
 import NavMenu from './NavMenu'
 
 storiesOf('NavMenu', module)
+    .addDecorator(StoryRouter())
     .add('NavMenu', () => <NavMenu />)
