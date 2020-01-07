2a03ef6decc61c54fb71adf485e6bf9ca9d10026
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-07 14:23:37 -0500

    working on getting functions to cycle through slides

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -44,65 +44,93 @@ const slides = [
     }
 ]
 
+
+// function plusDivs(n) {
+    //     showDivs(slideIndex += 1)
+    // }
+    
+var slideIndex = 1;
+    showSlides(slideIndex)
+
+function plusSlides(n) {
+    showSlides(slideIndex += n)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+  }
+}
+
+
+
+
 const Slider = (props) => {
     return(
 <div>
-    <div className='slide slide1'>
-    <p className='title'>{slides[0].title}</p>
-    <div className='carousel-container'>
-        <div className='image-1'>
-            <img src={slides[0].img1a}/>
-            <p>{slides[0].label1a}</p>
+    <div className='slideshow-container'>
+        <div className='slide slide1'>
+            <p className='title'>{slides[0].title}</p>
+            <div className='slide-container'>
+                <div className='image-1'>
+                    <img src={slides[0].img1a}/>
+                    <p>{slides[0].label1a}</p>
+                </div>
+                <div className='image-2'>
+                    <img src={imageFile2}/>
+                    <p>{slides[0].label1b}</p>
+                </div>
+                {/* <button onClick={(e) => alert('clicked')}> > </button> */}
         </div>
-        <div className='image-2'>
-            <img src={imageFile2}/>
-            <p>{slides[0].label1b}</p>
         </div>
-        <button onClick={(e) => alert('clicked')}> > </button>
-    </div>
-    </div>
-    <div className='slide slide2'>
-    <p className='title'>{slides[1].title}</p>
-    <div className='carousel-container'>
-        <div className='image-1'>
-            <img src={slides[1].img2a}/>
-            <p>{slides[1].label2a}</p>
+        <div className='slide slide2'>
+            <p className='title'>{slides[1].title}</p>
+            <div className='slide-container'>
+                <div className='image-1'>
+                    <img src={slides[1].img2a}/>
+                    <p>{slides[1].label2a}</p>
+                </div>
+                <div className='image-2'>
+                    <img src={slides[1].img2b}/>
+                    <p>{slides[1].label2b}</p>
+                </div>
+                {/* <button onClick={(e) => alert('clicked')}> > </button> */}
         </div>
-        <div className='image-2'>
-            <img src={slides[1].img2b}/>
-            <p>{slides[1].label2b}</p>
         </div>
-        <button onClick={(e) => alert('clicked')}> > </button>
-    </div>
-    </div>
-    <div className='slide slide3'>
-    <p className='title'>{slides[2].title}</p>
-    <div className='carousel-container'>
-        <div className='image-1'>
-            <img src={slides[2].img3a}/>
-            <p>{slides[2].label3a}</p>
+        <div className='slide slide3'>
+            <p className='title'>{slides[2].title}</p>
+            <div className='slide-container'>
+                <div className='image-1'>
+                    <img src={slides[2].img3a}/>
+                    <p>{slides[2].label3a}</p>
+                </div>
+                <div className='image-2'>
+                    <img src={slides[2].img3b}/>
+                    <p>{slides[2].label3b}</p>
+                </div>
+                {/* <button onClick={(e) => alert('clicked')}> > </button> */}
         </div>
-        <div className='image-2'>
-            <img src={slides[2].img3b}/>
-            <p>{slides[2].label3b}</p>
         </div>
-        <button onClick={(e) => alert('clicked')}> > </button>
-    </div>
-    </div>
-    <div className='slide slide4'>
-    <p className='title'>{slides[3].title}</p>
-    <div className='carousel-container'>
-        <div className='image-1'>
-            <img src={slides[3].img4b}/>
-            <p>{slides[3].label4a}</p>
+        <div className='slide slide4'>
+            <p className='title'>{slides[3].title}</p>
+            <div className='slide-container'>
+                <div className='image-1'>
+                    <img src={slides[3].img4b}/>
+                    <p>{slides[3].label4a}</p>
+                </div>
+                <div className='image-2'>
+                    <img src={slides[3].img4a}/>
+                    <p>{slides[3].label4b}</p>
+                </div>
+                {/* <button onClick={(e) => alert('clicked')}> > </button> */}
         </div>
-        <div className='image-2'>
-            <img src={slides[3].img4a}/>
-            <p>{slides[3].label4b}</p>
         </div>
-        <button onClick={(e) => alert('clicked')}> > </button>
     </div>
-    </div>
+        <button onClick={(e) => plusSlides}> &#10095; </button>
 </div>
     )
 
