944b74616adb158af74034bb7dac385006dc1f6e
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-06 23:26:16 -0500

    looks like aq dead end tonight, making a PR

--- client/src/components/Carousel/Carousel.stories.js
+++ client/src/components/Carousel/Carousel.stories.js
@@ -4,10 +4,41 @@ import imageFile2 from "../../Images/Fall-2020-2.png";
 
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import ReactDOM from 'react-dom';
+import { Carousel } from 'react-responsive-carousel';
+// import { render } from 'react-dom';
+// import Carousel from 'react-carousel';
 import './Carousel.css'
 import CarouselPics from './CarouselPics';
-var ReactDOM = require('react-dom');
-var Carousel = require('react-responsive-carousel').Carousel;
+
+
+// storiesOf('Carousel', module)
+//     .add('Gown Display', () => 
+//                 <Carousel>
+//                     <div>
+//                         <img src="{imageFile1}" />
+//                         <p className="legend">Legend 1</p>
+//                     </div>
+//                     <div>
+//                         <img src="assets/2.jpeg" />
+//                         <p className="legend">Legend 2</p>
+//                     </div>
+//                     <div>
+//                         <img src="assets/3.jpeg" />
+//                         <p className="legend">Legend 3</p>
+//                     </div>
+//                 </Carousel>
+//             );
+
+    // ReactDOM.render(<SlidesShow />, document.querySelector('.demo-carousel'));
+
+
+
+
+
+
+
+
 
 
 // const DemoCarousel = React.createClass({
@@ -53,21 +84,64 @@ var Carousel = require('react-responsive-carousel').Carousel;
 
 
 
+
+
+
+// const MyCarousel = ({ slideTime }) => {
+//     const carouselBehavior = useCarousel(slideTime);
+//     return <div className="my-carousel">...</div>;
+//   };
+
+// const [current, setCurrent] = React.useState(0);
+
+// React.useEffect(() => {
+//     const next = (current + 1) % slides.length;
+//     const id = setTimeout(() => setCurrent(next), time);
+//     return () => clearTimeout(id);
+//   }, [current]);
+
+
+
+
+// -------------
+
 storiesOf('Carousel', module)
-    .add('Gown Display', () => <div>
+    .add('Gown Display', () => 
+                                <div>
                                     <p className='title'>Fall 2020 Collection</p>
-                                    <div className='carousel-container'>
-                                        <div className='image-1'>
-                                            <img src={imageFile1}/>
+                                     <div className='carousel-container'>
+                                         <div className='image-1'>
+                                             <img src={imageFile1}/>
                                             <p>Luca</p>
-                                        </div>
-                                        <div className='image-2'>
-                                            <img src={imageFile2}/>
-                                            <p>Vida</p>
-                                        </div>
-                                        <button> > </button>
-                                    </div>
-                                </div>)
+                                         </div>
+                                         <div className='image-2'>
+                                             <img src={imageFile2}/>
+                                             <p>Vida</p>
+                                         </div>
+                                         <button onClick={(e) => alert('clicked')}> > </button>
+                                     </div>
+                                 </div>)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+{/* <div class="gallery-container">
+    <ul class="gallery js-gallery">
+      <li class="gallery__item js-gallery-item" style="background-image: url(`imageFile1`)"></li>
+      <li class="gallery__item js-gallery-item" style="background-image: url(`imageFile1`)"></li>
+      <li class="gallery__item js-gallery-item" style="background-image: url(`imageFile1`)"></li>
+      <li class="gallery__item js-gallery-item" style="background-image: url(`imageFile1`)"></li>
+    </ul>
+</div> */}
+    
 
 
 
