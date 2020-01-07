9f5e893d31661615e49b2db3edacd304a4c7bfeb
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-07 15:10:18 -0500

    formatted with prettier

--- client/src/components/NavMenu/NavMenu.stories.js
+++ client/src/components/NavMenu/NavMenu.stories.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import StoryRouter from 'storybook-react-router';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import StoryRouter from "storybook-react-router";
 
-import NavMenu from './NavMenu'
+import NavMenu from "./NavMenu";
 
-storiesOf('NavMenu', module)
-    .addDecorator(StoryRouter())
-    .add('NavMenu', () => <NavMenu />)
+storiesOf("NavMenu", module)
+  .addDecorator(StoryRouter())
+  .add("NavMenu", () => <NavMenu />);
