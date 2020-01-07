1d96ebbdfef0b63bb5f1f10147c9dc4c7220ebba
Jose Casado <jcasado6@yahoo.com>
2020-01-07 17:06:40 -0500

    Revert "changed around class names and organized"
    This reverts commit e428e48b63548d527d3fc04f322e5f21311afe42.

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,26 +1,21 @@
-import React from "react";
-import "./Button.css";
+import React from 'react';
+import './Button.css'
 
-const Button = props => {
-  let classList = "button";
-  let types = [
-    "light-pink",
-    "dark-pink",
-    "small-grey",
-    "red",
-    "grey",
-    "white"
-  ];
 
-  if (types.includes(props.type)) {
-    classList += ` button-${props.type} `;
-  }
 
-  if (props.large) {
-    classList += ` button-large`;
-  }
+const Button = (props) => {
+    let classList = 'button'
+    let types = ['light-pink', 'dark-pink', 'small-grey', 'large-red', 'large-grey', 'large-white']
 
-  return <button className={classList}> {props.label}</button>;
-};
+    if (types.includes(props.type)) {
+        classList += ` button-${props.type} `
+    }
 
-export default Button;
+    if (props.large) {
+        classList += ` large-red, large-grey, large white`
+    }
+
+    return <button className={classList}> {props.label}</button>
+}
+
+export default Button
