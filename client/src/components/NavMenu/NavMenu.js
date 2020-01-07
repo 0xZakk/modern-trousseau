2a96ae86f782abb10d16f8ed250bb364b85d0121
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-07 10:31:10 -0500

    added in props and links

--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -2,40 +2,45 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import './NavMenu.css'
 
-class NavMenu extends Component {
-
-    render() {
-        return (
-            <div className="nav-container">
-                <nav>
-                    <ul>
-                        <li>
-                                <h3>Home</h3>
+const NavMenu = (props) => (
+    props.isVisible ?
+        <div className="nav-container">
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="#">
+                            <h3 className="nav-title">Home</h3>
                             <span className="dots">&#9679;</span>
-                        </li>
-                        <li>
-                         
-                                <h3>Our Gowns</h3>
-                            <span className="dots">&#9679;</span>
-                        </li>
-                        <li>
-                            <h3>Visit Us</h3>
-                            <span className="dots">&#9679;</span>
-                        </li>
-                        <li>
-                                <h3>Book An Appointment</h3>
-                            <span className="dots">&#9679;</span>
-                        </li>
-                        <li>
-                           <h3>About Us</h3>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        )
-    }
-};
-
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                        <h3 className="nav-title">Our Gowns</h3>
+                        <span className="dots">&#9679;</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                        <h3 className="nav-title">Visit Us</h3>
+                        <span className="dots">&#9679;</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                        <h3 className="nav-title">Book An Appointment</h3>
+                        <span className="dots">&#9679;</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="#">
+                        <h3 className="nav-title">About Us</h3>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        : null
+);
 
 
 export default NavMenu;
