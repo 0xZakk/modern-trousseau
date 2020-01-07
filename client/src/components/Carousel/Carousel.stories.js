6e8471aa394e885f042947b3d61f915186aa379f
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-06 20:56:36 -0500

    back to static picture layout

--- client/src/components/Carousel/Carousel.stories.js
+++ client/src/components/Carousel/Carousel.stories.js
@@ -1,66 +1,73 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import CarouselPics from './CarouselPics';
-var Carousel = require('react-responsive-carousel').Carousel;
-
 import imageFile1 from "../../Images/Fall-2020-1.png";
 import imageFile2 from "../../Images/Fall-2020-2.png";
+
+
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import './Carousel.css'
+import CarouselPics from './CarouselPics';
+var ReactDOM = require('react-dom');
+var Carousel = require('react-responsive-carousel').Carousel;
+
 
 // const DemoCarousel = React.createClass({
 //     render() {
 //         return (
 
 
-storiesOf('Carousel', module)
-    .add('Gown Display', () => 
-            <Carousel showArrows={true} onClick={(e) => alert('clicked')    }>
-                <div>
-                    <img src={imageFile1} />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src={imageFile2} />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src={imageFile1} />
-                    <p className="legend">Legend 3</p>
-                </div>
-                <div>
-                    <img src={imageFile2} />
-                    <p className="legend">Legend 4</p>
-                </div>
-                <div>
-                    <img src={imageFile1} />
-                    <p className="legend">Legend 5</p>
-                </div>
-                <div>
-                    <img src={imageFile2} />
-                    <p className="legend">Legend 6</p>
-                </div>
-            </Carousel>
-        );
+// storiesOf('Carousel', module)
+//     .add('Gown Display', () => 
+//             <Carousel showArrows={true} onClick={(e) => alert('clicked') }>
+//                 <div>
+//                     <img src={imageFile1} />
+//                     <p className="legend">Legend 1</p>
+//                 </div>
+//                 <div>
+//                     <img src={imageFile2} />
+//                     <p className="legend">Legend 2</p>
+//                 </div>
+//                 <div>
+//                     <img src={imageFile1} />
+//                     <p className="legend">Legend 3</p>
+//                 </div>
+//                 <div>
+//                     <img src={imageFile2} />
+//                     <p className="legend">Legend 4</p>
+//                 </div>
+//                 <div>
+//                     <img src={imageFile1} />
+//                     <p className="legend">Legend 5</p>
+//                 </div>
+//                 <div>
+//                     <img src={imageFile2} />
+//                     <p className="legend">Legend 6</p>
+//                 </div>
+//             </Carousel>
+//         );
+
+
+
 //     }
 // });
 // ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
 
 
 
-// storiesOf('Carousel', module)
-//     .add('Gown Display', () => <div>
-//                                     <p className='title'>Fall 2020 Collection</p>
-//                                     <div className='carousel-container'>
-//                                         <div className='image-1'>
-//                                             <img src={imageFile1}/>
-//                                             <p>Luca</p>
-//                                         </div>
-//                                         <div className='image-2'>
-//                                             <img src={imageFile2}/>
-//                                             <p>Vida</p>
-//                                         </div>
-//                                         <button> > </button>
-//                                     </div>
-//                                 </div>)
+storiesOf('Carousel', module)
+    .add('Gown Display', () => <div>
+                                    <p className='title'>Fall 2020 Collection</p>
+                                    <div className='carousel-container'>
+                                        <div className='image-1'>
+                                            <img src={imageFile1}/>
+                                            <p>Luca</p>
+                                        </div>
+                                        <div className='image-2'>
+                                            <img src={imageFile2}/>
+                                            <p>Vida</p>
+                                        </div>
+                                        <button> > </button>
+                                    </div>
+                                </div>)
 
 
 
