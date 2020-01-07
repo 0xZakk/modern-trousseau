d8436e0000604757e98f3eb99bff58616ae0fd0a
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-06 20:29:22 -0500

    back to where I was several tries bac

--- client/src/components/Carousel/Carousel.stories.js
+++ client/src/components/Carousel/Carousel.stories.js
@@ -1,76 +1,89 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { storiesOf } from '@storybook/react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import styled from 'styled-components';
-import { storiesOf } from '@storybook/react';
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import { Gallery, GalleryImage } from "react-gesture-gallery";
 import CarouselPics from './CarouselPics';
 
-import Fall1 from '../../Images/Fall-2020-1.png';
-import Fall2 from '../../Images/Fall-2020-2.png';
-import Spring1 from '../../Images/Spring-2020-1.jpeg';
-import Spring2 from '../../Images/Spring-2020-2.jpeg';
-import Trending1 from '../../Images/Trending-Now-1.jpeg';
-import Trending2 from '../../Images/Trending-Now-2.jpeg';
-import Classics1 from '../../Images/Classics-1.jpeg';
-import classics2 from '../../Images/Classics-2.jpeg';
+import imageFile1 from "../../Images/Fall-2020-1.png";
+import imageFile2 from "../../Images/Fall-2020-2.png";
 
-const images = [
-    Fall1,
-    Fall2,
-    Spring1,
-    Spring2,
-    Trending1,
-    Trending2,
-    Classics1,
-    classics2
-]
+storiesOf('Carousel', module).add('Gown Display', () => 
+    
+    class DemoCarousel extends Component {
+        render() {
+            return (
+                <Carousel>
+                    <div>
+                        <img src="assets/1.jpeg" />
+                        <p className="legend">Legend 1</p>
+                    </div>
+                    <div>
+                        <img src="assets/2.jpeg" />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                    <div>
+                        <img src="assets/3.jpeg" />
+                        <p className="legend">Legend 3</p>
+                    </div>
+                </Carousel>
+            );
+        }
+    });
 
-// var ReactDOM = require('react-dom');
-// var Carousel = require('react-responsive-carousel').Carousel;
+     
+    ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
+    
+                                // <div>
+                                //     <p className='title'>Fall 2020 Collection</p>
+                                //     <div className='carousel-container'>
+                                //         <div className='image-1'>
+                                //             <img src="../../Images/Fall-2020-1.png"/>
+                                //             <p>Luca</p>
+                                //         </div>
+                                //         <div className='image-2'>
+                                //             <img src="../../Images/Fall-2020-2.png"/>
+                                //             <p>Vida</p>
+                                //         </div>
+                                //         <button> > </button>
+                                //     </div>
+                                // </div>)
 
-// import { Carousel } from 'react-responsive-carousel';
-// import Carousel from 'components/DemoCarousel'
 
-function handleClick(e) {
-    e.preventDefault();
-    alert("You clicked the button")
-}
-// const [index, setIndex] = React.useState(0);
 
-// const images = [
-//   {src: Fall1},
-//   {src: Fall2},
-//   {src: Spring1},
-//   {src: Spring2},
-//   {src: Trending1},
-//   {src: Trending2},
-//   {src: Classics1},
-//   {src: Classics2}
-// ];
 
-storiesOf('Carousel', module).add('Gown Display', () =>
-    class GownCarousel extends Component() {
-       render() {
-        return (
-            <Carousel>
-            <div>
-                <img src="assets/1.jpeg" />
-                <p className="legend">Legend 1</p>
-            </div>
-            <div>
-                <img src="assets/2.jpeg" />
-                <p className="legend">Legend 2</p>
-            </div>
-            <div>
-                <img src="assets/3.jpeg" />
-                <p className="legend">Legend 3</p>
-            </div>
-        </Carousel>
-    );
-}
-    }
-    )
+
+
+
+
+
+    .add('Gown Display with Carousel Package', () => <div className='carousel-container'>
+                                    <Carousel>
+                                        <div className='image-1'>
+                                            <img src=''/>
+                                            <p>fsf</p>
+                                        </div>
+                                        {/* <div className='image-2'>
+                                            <img src=''/>
+                                            <p>text</p>
+                                        </div> */}
+                                        {/* <Arrow right onClick={handleClick} data-position={position + 1}>{'>'}</Arrow> */}
+                                    </Carousel>
+                                    {/* <Arrow onClick={handleClick} data-position={position - 1}>{'<'}</Arrow> */}
+                                </div>)
+                    
+
+    .add('Gown Display from codepen', () => <div className='carousel-container'>
+                                                <Carousel>
+                                                    <div className='image-1'>
+                                                        <img src=''/>
+                                                        <p>fsf</p>
+                                                    </div>
+                                                    {/* <div className='image-2'>
+                                                        <img src=''/>
+                                                        <p>text</p>
+                                                    </div> */}
+                                                    {/* <Arrow right onClick={handleClick} data-position={position + 1}>{'>'}</Arrow> */}
+                                                </Carousel>
+                                                {/* <Arrow onClick={handleClick} data-position={position - 1}>{'<'}</Arrow> */}
+                                            </div>)
+                                
