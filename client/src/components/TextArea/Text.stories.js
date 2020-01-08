ef051a431c5a66fc4471277d6d569446de618930
bukharov <dilmurod.bukharov@gmail.com>
2020-01-08 18:29:15 -0500

    cloned down repo and changed the branch and copy pasted the code I already wrote

--- client/src/components/TextArea/Text.stories.js
+++ client/src/components/TextArea/Text.stories.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Text from "./Text";
 
-import Text from './Text';
-
-storiesOf('Text', module)
-    .add('Default', () => <Text pClassList='text-area-title' textAreaClassList='text-area'/>)
+storiesOf("Text", module).add("Default", () => (
+  <Text pClassList="text-area-title" textAreaClassList="text-area" />
+));
