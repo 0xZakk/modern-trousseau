ad0d6617995a5ae43ef0563a0eac5b2a7521d897
Adam_Bates <Creizai@EMAIL.COM>
2020-01-08 14:07:40 -0500

    refactored functional into class so I can use state in a way I can deal OnChange

--- client/src/components/TextInputComponent/TextInput.js
+++ client/src/components/TextInputComponent/TextInput.js
@@ -1,15 +1,34 @@
 import React from "react";
 import "./TextInput.css";
 
-const TextInput = props => (
-  <>
-    <label className="label-name">{props.label}</label>
-    <input
-      className="text-input"
-      type={props.type}
-      placeholder={props.placeholder}
-    />
-  </>
-);
+class TextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <label className="label-name">{this.props.label}</label>
+        <input
+          className="text-input"
+          type={this.props.type}
+          placeholder={this.props.placeholder}
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </>
+    );
+  }
+}
 export default TextInput;
+
+// value={this.state.value}
+// onChange={this.handleChange}
