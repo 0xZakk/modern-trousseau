1aa9e86c68b23a781a84fc2e560dc72b0d306e9d
Adam DeLessio <adamdelessio@gmail.com>
2020-01-08 12:35:47 -0500

    attempted to make corrections

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import './Card.css'
-import SingleCard from './SingleCard'
 
-const Card = props => {
-	let cards = [
-		{
-			// icon: ,
-			title: 'Get Inspired',
-			p: 'Each design evokes classic couture while expressing a vision of modern style & sophistication.'
-		},
-		{
-			// icon: ,
-			title: 'Book With Us',
-			p: 'Spend an hour with us to create a truly unique, personalized & flattering wedding dress.'
-		},
-		{
-			// icon: ,
-			title: 'Create Your Dream Dress',
-			p: 'Anything and everything is customizable to make you feel confident & beautiful.'
-		}
-	]
-
-	let cardMap = cards.map((card, index) =>  {
-		return(
-			<SingleCard title={card.title} p={card.p} key={index} />
-		)
-	})
+const Card = (props) => {
 	return(
 		<div className='card-container'>
-			<h2 className='title'>How We Work</h2>
-			{cardMap}
+			<Icon type='heart' />
+			<h4 className='card-title'>Get Inspired</h4>
+			<p className='card-p'>Each design evokes classic couture while expressing a vision of modern style & sophistication.</p>
 		</div>
 	)
 }
