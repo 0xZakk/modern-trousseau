5d563c92abc86aa2fe73e8216ef3146a64fc041b
Adam_Bates <Creizai@EMAIL.COM>
2020-01-08 10:05:25 -0500

    Fixed the TextInput Brackets in stories to clean up the code

--- client/src/components/TextInputComponent/TextInput.stories.js
+++ client/src/components/TextInputComponent/TextInput.stories.js
@@ -5,34 +5,21 @@ import TextInput from "./TextInput";
 
 storiesOf("TextInput", module)
   .add("Primary", () => (
-    <TextInput
-      children={"blah"}
-      labelName={""}
-      type={"text"}
-      placeholder={"Sign up here"}
-    ></TextInput>
+    <TextInput labelName={""} type={"text"} placeholder={"Sign up here"} />
   ))
   .add("Full Name", () => (
-    <TextInput
-      labelName={"Full Name*"}
-      type={"text"}
-      placeholder={""}
-    ></TextInput>
+    <TextInput labelName={"Full Name*"} type={"text"} placeholder={""} />
   ))
   .add("Email", () => (
-    <TextInput labelName={"Email*"} type={"email"} placeholder={""}></TextInput>
+    <TextInput labelName={"Email*"} type={"email"} placeholder={""} />
   ))
   .add("Phone Number", () => (
-    <TextInput
-      labelName={"Phone Number*"}
-      type={"number"}
-      placeholder={""}
-    ></TextInput>
+    <TextInput labelName={"Phone Number*"} type={"number"} placeholder={""} />
   ))
   .add("Number of Guests", () => (
     <TextInput
       labelName={"Number of Guests"}
       type={"number"}
       placeholder={""}
-    ></TextInput>
+    />
   ));
