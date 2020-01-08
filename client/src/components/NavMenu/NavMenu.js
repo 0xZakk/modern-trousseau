2064f53e216259282ceb9a121176f6c54b8f4557
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-08 12:32:39 -0500

    Revert "added in props and links"
    This reverts commit 2a96ae86f782abb10d16f8ed250bb364b85d0121.

--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -2,45 +2,40 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import './NavMenu.css'
 
-const NavMenu = (props) => (
-    props.isVisible ?
-        <div className="nav-container">
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="#">
-                            <h3 className="nav-title">Home</h3>
+class NavMenu extends Component {
+
+    render() {
+        return (
+            <div className="nav-container">
+                <nav>
+                    <ul>
+                        <li>
+                                <h3>Home</h3>
                             <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">Our Gowns</h3>
-                        <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">Visit Us</h3>
-                        <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">Book An Appointment</h3>
-                        <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">About Us</h3>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        : null
-);
+                        </li>
+                        <li>
+                         
+                                <h3>Our Gowns</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                            <h3>Visit Us</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                                <h3>Book An Appointment</h3>
+                            <span className="dots">&#9679;</span>
+                        </li>
+                        <li>
+                           <h3>About Us</h3>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+};
+
 
 
 export default NavMenu;
