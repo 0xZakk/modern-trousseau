b3652c2ef4e6347aab4abf2dae689089b27b5bcf
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-08 15:32:32 -0500

    title and text created for mobile default

--- client/src/components/Bridal-Appt./Bridal-Appt.js
+++ client/src/components/Bridal-Appt./Bridal-Appt.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const BridalAppt = () => (
-    <div>
-        
+  <div>
+    <h4>Bridal Appointment</h4>
+    <div className="main-text">
+      Modern Trousseau appointments are truly about you, the bride. Our Flagship
+      stores offer a private appointment for you, your family and friends, to
+      create your dream wedding gown. With every Modern Trousseau gown made in
+      Woodbridge, Connecticut, be prepared for fun, bubbles, and that magic
+      moment where you find your dream dress.
     </div>
-
+  </div>
 );
 
 export default BridalAppt;
