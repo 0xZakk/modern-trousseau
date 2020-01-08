e805f8f5c4b8f612e38633b7c38e78f82192f192
Kelly M Collins <90kcol@gmail.com>
2020-01-08 10:46:48 -0500

    added more icons and changed some names

--- client/src/components/Icons/FAIcon.js
+++ client/src/components/Icons/FAIcon.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+library.add(fab, fas, far)
 
-library.add(fab)
-
-const FontIcon = (props) => (
+const Icons = (props) => (
     <div>
         <FontAwesomeIcon icon = {props.name}/>
     </div>
 )
 
-export default FontIcon;
+export default Icons;
--- client/src/components/Icons/FAIcon.stories.js
+++ client/src/components/Icons/FAIcon.stories.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 
-import FontIcon from './FAIcon'
+import Icons from './FAIcon'
 
-storiesOf('FontIcon', module)
-    .add('PininterestIcon', () => <FontIcon name={['fab', 'pinterest']}/>)
-    .add('FacebookIcon', () => <FontIcon name={['fab', 'facebook']}/>)
-    .add('InstagramIcon', () => <FontIcon name={['fab', 'instagram']}/>)
+storiesOf('Icons', module)
+    .add('Left-Arrow', () => <Icons name={['fas', 'arrow-left']}/>)
+    .add('Chevron-Left', () => <Icons name={['fas', 'chevron-left']}/>)
+    .add('Chevron-Right', () => <Icons name={['fas', 'chevron-right']}/>)
+    .add('Sort', () => <Icons name={['fas', 'sort']}/>)
+    .add('Hand', () => <Icons name={['far', 'hand-pointer']}/>)
+    .add('Menu', () => <Icons name={['fas', 'bars']}/>)
+    .add('User', () => <Icons name={['far', 'user']}/>)
+    .add('Pinterest', () => <Icons name={['fab', 'pinterest']}/>)
+    .add('Facebook', () => <Icons name={['fab', 'facebook']}/>)
+    .add('Instagram', () => <Icons name={['fab', 'instagram']}/>)
+    .add('Square', () => <Icons name={['far', 'square']}/>)
+    .add('Check-Square', () => <Icons name={['far', 'check-square']}/>)
+    .add('Plus-Square', () => <Icons name={['far', 'plus-square']}/>)
+    .add('Calendar', () => <Icons name={['fas', 'calendar-day']}/>)
+    .add('Heart', () => <Icons name={['far', 'heart']}/>)
+    .add('Search', () => <Icons name={['fas', 'search']}/>)
+    .add('Phone', () => <Icons name={['fas', 'phone-alt']}/>)
+    .add('Email', () => <Icons name={['far', 'envelope']}/>)
+    .add('Time', () => <Icons name={['far', 'clock']}/>)
+    .add('Location', () => <Icons name={['fas', 'map-marker-alt']}/>)
+    .add('Minus', () => <Icons name={['fas', 'minus']}/>)
+    .add('Plus', () => <Icons name={['fas', 'plus']}/>)
+    .add('Times', () => <Icons name={['fas', 'times']}/>)
