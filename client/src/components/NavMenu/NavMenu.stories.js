70509aeadf38c94fdcad71833fcb2f4c05eb695f
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-08 09:10:03 -0500

    changed made

--- client/src/components/NavMenu/NavMenu.stories.js
+++ client/src/components/NavMenu/NavMenu.stories.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import StoryRouter from "storybook-react-router";
+//import StoryRouter from "storybook-react-router";
 
 import NavMenu from "./NavMenu";
 
 storiesOf("NavMenu", module)
-  .addDecorator(StoryRouter())
+  //.addDecorator(StoryRouter())
   .add("NavMenu", () => <NavMenu />);
