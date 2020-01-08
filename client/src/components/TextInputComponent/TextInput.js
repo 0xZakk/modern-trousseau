89123ef74b4ef8f42200f86f665093239b5d5c7f
Adam_Bates <Creizai@EMAIL.COM>
2020-01-08 10:03:12 -0500

    added label and props to pass it through

--- client/src/components/TextInputComponent/TextInput.js
+++ client/src/components/TextInputComponent/TextInput.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./TextInput.css";
 
-const TextInput = props => <input type="text" placeholder={props.label} />;
+const TextInput = props => (
+  <>
+    <label>{props.labelName}</label>
+    <br />
+    <input type={props.type} placeholder={props.placeholder} />
+  </>
+);
 
 export default TextInput;
--- client/src/components/TextInputComponent/TextInput.stories.js
+++ client/src/components/TextInputComponent/TextInput.stories.js
@@ -3,6 +3,36 @@ import { storiesOf } from "@storybook/react";
 
 import TextInput from "./TextInput";
 
-storiesOf("TextInput", module).add("Primary", () => (
-  <TextInput label={"Sign up here"}></TextInput>
-));
+storiesOf("TextInput", module)
+  .add("Primary", () => (
+    <TextInput
+      children={"blah"}
+      labelName={""}
+      type={"text"}
+      placeholder={"Sign up here"}
+    ></TextInput>
+  ))
+  .add("Full Name", () => (
+    <TextInput
+      labelName={"Full Name*"}
+      type={"text"}
+      placeholder={""}
+    ></TextInput>
+  ))
+  .add("Email", () => (
+    <TextInput labelName={"Email*"} type={"email"} placeholder={""}></TextInput>
+  ))
+  .add("Phone Number", () => (
+    <TextInput
+      labelName={"Phone Number*"}
+      type={"number"}
+      placeholder={""}
+    ></TextInput>
+  ))
+  .add("Number of Guests", () => (
+    <TextInput
+      labelName={"Number of Guests"}
+      type={"number"}
+      placeholder={""}
+    ></TextInput>
+  ));
