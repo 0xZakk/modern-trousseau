738a8588be05e6a23c6f6efdb62d63a2a27df113
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-08 12:28:37 -0500

    Revert "added in storybook router to navmenu.stories.js"
    This reverts commit 7373d61ae8dbe44fc5d566fb020813d02bb9f45f.

--- client/src/components/NavMenu/NavMenu.stories.js
+++ client/src/components/NavMenu/NavMenu.stories.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import StoryRouter from 'storybook-react-router';
 
 import NavMenu from './NavMenu'
 
 storiesOf('NavMenu', module)
-    .addDecorator(StoryRouter())
     .add('NavMenu', () => <NavMenu />)
