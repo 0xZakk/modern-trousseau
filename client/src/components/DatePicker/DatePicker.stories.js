a32906d2a042912b3ad49dcf762a26fefe6c67c3
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-08 11:23:14 -0500

    Readding DatePicker modified.

---
+++ client/src/components/DatePicker/DatePicker.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import DatePicker from './DatePicker';
+
+function Child(props) {
+    function handleChange(event) {
+        // Child component called by parent with props.onChange
+        props.onChange(event.target.value);
+    }
+  
+    return <input value={props.value} onChange={handleChange} />
+}
+
+storiesOf('DatePicker', module)
+    .add('DatePicker', () => <DatePicker label={"Requested Date*"} value={"November 11"} min={"February 29, 2020"} step="1" max={"February 28, 2022") />)
