82f1d559eaa3458389c49925d6b519969ef0ca8f
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-08 16:59:43 -0500

    head

--- client/src/components/NavMenu/NavMenu.js
+++ client/src/components/NavMenu/NavMenu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavMenu.css";
@@ -44,51 +43,3 @@ const NavMenu = props => (
 );
 
 export default NavMenu;
-=======
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import './NavMenu.css'
-
-const NavMenu = (props) => (
-    props.isVisible ?
-        <div className="nav-container">
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="#">
-                            <h3 className="nav-title">Home</h3>
-                            <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">Our Gowns</h3>
-                        <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">Visit Us</h3>
-                        <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">Book An Appointment</h3>
-                        <span className="dots">&#9679;</span>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="#">
-                        <h3 className="nav-title">About Us</h3>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        : null
-);
-
-
-export default NavMenu;
->>>>>>> storybook-router
