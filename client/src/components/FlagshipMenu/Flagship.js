c6596ec2f95e27edcec34f9f0d37874890808605
Adam DeLessio <adamdelessio@gmail.com>
2020-01-08 14:38:36 -0500

    renamed files

---
+++ client/src/components/FlagshipMenu/Flagship.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import './Flagship.css'
+
+
+
+
+
+export default Flagship
---
+++ client/src/components/FlagshipMenu/Flagship.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Flagship from './Flagship';
+
+storiesOf('Flagship Menu', module)
+    .add('Primary', () => <Button>This is a Button</Button>)
