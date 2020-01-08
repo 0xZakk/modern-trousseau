fa0d81269a6aa0e76ea979879a92b0fb3e2f1ecf
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-08 12:24:18 -0500

    Changed function handleChange in DatePicker.stories.js.  Passed function and label properties as props.:

--- client/src/components/DatePicker/DatePicker.js
+++ client/src/components/DatePicker/DatePicker.js
@@ -2,19 +2,11 @@ import React from 'react'
 import './DatePicker.css'
 import imageFile from '../../images/Search Icon_outlined.svg'
 
-function Child(props) {
-    function handleChange(event) {
-        // Parent calls Child and function onChange
-        props.onChange(event.target.value);
-    }
-  
-    return <input value={props.value} onChange={handleChange} />
-}
 
 const DatePicker = (props) => 
 <div>
-<label className="date-picker" for='requested-date-input'>{props.label}</label>
-<input type="date" className="requested-date-input" name='Requested Date' value={props.value} min={props.min} step="1" max={props.max)/>
+  <label className="date-picker" for='requested-date-input'>{props.label}</label>
+  <input type="date" className="requested-date-input" name={props.name} value={props.value} min={props.min} step={props.step} max={props.max} onChange={props.onChange}/>
 </div>  
 
 export default DatePicker
