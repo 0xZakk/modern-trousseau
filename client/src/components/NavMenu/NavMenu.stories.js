746f177f195459db50725cd802ee781d90f99664
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-08 17:01:34 -0500

    fix

--- client/src/components/NavMenu/NavMenu.stories.js
+++ client/src/components/NavMenu/NavMenu.stories.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
@@ -6,12 +5,3 @@ import NavMenu from "./NavMenu";
 
 storiesOf("NavMenu", module)
   .add("NavMenu", () => <NavMenu />);
-=======
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import NavMenu from './NavMenu'
-
-storiesOf('NavMenu', module)
-    .add('NavMenu', () => <NavMenu />)
->>>>>>> storybook-router
