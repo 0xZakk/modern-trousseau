6f75d075acea356c99135630973e447680d8765a
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-09 15:01:45 -0500

    created .stories.js file

---
+++ client/src/components/BridalAppointment/BridalAppointment.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import BridalAppointment from './BridalAppointment'
+
+storiesOf('BridalAppointment', module)
+    .add('BridalAppointment', () => <BridalAppointment />)
