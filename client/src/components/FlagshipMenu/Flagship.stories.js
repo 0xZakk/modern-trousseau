5e3b5cadff24b5c8636201fe100cadd7484b43b6
Adam DeLessio <adamdelessio@gmail.com>
2020-01-09 10:58:12 -0500

    added some data

--- client/src/components/FlagshipMenu/Flagship.stories.js
+++ client/src/components/FlagshipMenu/Flagship.stories.js
@@ -4,4 +4,10 @@ import Menu from './Flagship';
 
 
 storiesOf('Flagship Menu', module)
-    .add('Primary', () => (<Menu city='' image='' address='' phone='' email='' instagram='' />))
+    .add('Primary', () => (<Menu city='Baltimore, MD'
+    							 image=''
+    							 address='1429 Aliceanna Street Baltimore, MD 21231' 
+    							 phone='(443) 447-8929' 
+    							 email='baltimore@moderntrousseau.com' 
+    							 instagram='Need more inspo? Follow us at @moderntrousseaubaltimore'
+    						/>))
