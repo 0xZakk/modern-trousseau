d42ccea4994adfed961a3d6f3466e965ea643bb1
AlexGarcia <afgarcia666@gmail.com>
2020-01-09 12:08:15 -0500

    got function to work with classes, got it to change icons but now it won't change back fml

--- client/src/components/Hamburger/Hamburger.js
+++ client/src/components/Hamburger/Hamburger.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -6,20 +6,45 @@ import './Hamburger.css';
 
 library.add(faBars, faTimes)
 
-const Hamburger = (props) => {
-    let classList = "hamburger";
-    
-    let iconList = `${props.icon}`;
-    
-    let types = ['Bars', 'Times'];
-    if (types.includes(props.type)) {
-        classList += ` hamburger-${props.type}`
+class Hamburger extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            open: false,
+            iconList: "bars",
+            typeName: "Bars"
+        }
+        this.openMenu = this.openMenu.bind(this)
     }
+    // let iconList = "bars"
+    // let classList = "";
+    // let classList = "hamburger";
+    
+    // let iconList = `${props.icon}`;
+    
+    // let types = "icon";
+    // let types = "icon";
+    // if (types.includes(props.type)) {
+    //     classList += ` hamburger-${props.type}`
+    // }
+    openMenu() {
+        this.setState({
+        ...this.state,
+        iconList: "times",
+        open: !this.state.open})
+        console.log(this.state.iconList + " " + this.state.classList);
+        }
+    render() {
     return (
-    <button className="hamburger-menu" onClick={props.onClick}>
-        <FontAwesomeIcon icon={iconList} className={classList}/>
+    <button className="hamburger-menu" onClick={this.openMenu}>
+        <FontAwesomeIcon
+        open={this.state.open}
+        icon={this.state.iconList} 
+        className="hamburger-icon" 
+        />
     </button>
     )
+    }
 };
 
 export default Hamburger;
--- client/src/components/Hamburger/Hamburger.stories.js
+++ client/src/components/Hamburger/Hamburger.stories.js
@@ -3,14 +3,35 @@ import { storiesOf } from '@storybook/react';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Hamburger from './Hamburger';
 
-let typeName = "Bars";
-let iconName = "bars"
+// let typeName = "Bars";
+// let iconName = "bars"
 
-function handleClick() {
-    // documentElement.classList.add(".times")
-}
+// function toggle(e) {
+//     e.preventDefault()
+//     if (iconName === "bars") {
+//         iconName = "times"
+//     } else {
+
+//     }
+// }
+
+// function toggle(e) {
+//     e.preventDefault();
+//     if (iconName === "bars"){
+//         iconName = "times"
+//         typeName = "Times"
+//         alert(iconName + typeName);
+//     }else{
+//         iconName = "bars"
+//         typeName = "Bars"
+//         alert(typeName);
+//     }
+    
+    
+// }
+
 
 storiesOf('Hamburger', module)
     .add('Hamburger', () => 
-    <Hamburger type={typeName} icon={iconName} onClick={handleClick}>
+    <Hamburger type="icon" icon="bars">
     </Hamburger>)
