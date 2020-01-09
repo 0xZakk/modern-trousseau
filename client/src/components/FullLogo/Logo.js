fbd6ea031a2ce1fd8aebac3c10ddbd44100a8a9c
robertsbaer <robertsbaer@yahoo.com>
2020-01-09 15:53:52 -0500

    fixed indentation

--- client/src/components/FullLogo/Logo.js
+++ client/src/components/FullLogo/Logo.js
@@ -1,24 +1,25 @@
-import React from 'react'
-import './Logo.css'
+import React from "react";
+import "./Logo.css";
 
-const Logo = (props) => {
-    
-    let classList = 'logo-tag'
+const Logo = props => {
+  let classList = "logo-tag";
 
-    let types = ['logo','no-tag']
+  let types = ["logo", "no-tag"];
 
-    if (types.includes(props.type)) {
-        classList += ` ${props.type}`
-    }
+  if (types.includes(props.type)) {
+    classList += ` ${props.type}`;
+  }
 
-return <>
-<div className="logo-tag">
-    <div className="logo">
-        <div className="modern">Modern</div>
-        <div className="trouseau">trousseau</div>
-    </div>
-    <div className={classList}>designed & made with love in the usa</div>
-</div>
-</>
-}
-export default Logo
+  return (
+    <>
+      <div className="logo-tag">
+        <div className="logo">
+          <div className="modern">Modern</div>
+          <div className="trouseau">trousseau</div>
+        </div>
+        <div className={classList}>designed & made with love in the usa</div>
+      </div>
+    </>
+  );
+};
+export default Logo;
--- client/src/components/FullLogo/Logo.stories.js
+++ client/src/components/FullLogo/Logo.stories.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
+import React from "react";
+import { storiesOf } from "@storybook/react";
 
-import Logo from './Logo';
+import Logo from "./Logo";
 
-storiesOf('Logo', module)
-    .add('Logo', () => <Logo />)
-    .add('logo-without-tag', () => <Logo 
-        type="no-tag"
-    />)
+storiesOf("Logo", module)
+  .add("Logo", () => <Logo />)
+  .add("Logo without tag", () => <Logo type="no-tag" />);
