28bbc712c2d48046ff351f0dc1501936c5b61308
Jose Casado <jcasado6@yahoo.com>
2020-01-09 16:50:08 -0500

    working without needed components

--- client/src/components/SingupCard/SignupCard.js
+++ client/src/components/SingupCard/SignupCard.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./SignupCard.css";
 
 const SignupCard = props => (
-  <div className='signup'>
-    Obsessed with a dress?
+  <div className="signup">
+    <div className="container">
+      Obsessed with a dress?<form placeholder='sign up for updates'></form><button className="small-pink">Submit</button>
+    </div>
   </div>
 );
 
