c3898c881847e7fb8ad1f6a3b9840e980202504c
Adam_Bates <Creizai@EMAIL.COM>
2020-01-09 09:40:47 -0500

    thought i pushed this already

--- client/src/components/TextInputComponent/TextInput.js
+++ client/src/components/TextInputComponent/TextInput.js
@@ -1,34 +1,18 @@
 import React from "react";
 import "./TextInput.css";
 
-class TextInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
+const TextInput = props => {
+  return (
+    <>
+      <label className="label-name">{props.label}</label>
+      <input
+        className="text-input"
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+      />
+    </>
+  );
+};
 
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  render() {
-    return (
-      <>
-        <label className="label-name">{this.props.label}</label>
-        <input
-          className="text-input"
-          type={this.props.type}
-          placeholder={this.props.placeholder}
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-      </>
-    );
-  }
-}
 export default TextInput;
-
-// value={this.state.value}
-// onChange={this.handleChange}
--- client/src/components/TextInputComponent/TextInput.stories.js
+++ client/src/components/TextInputComponent/TextInput.stories.js
@@ -2,19 +2,47 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import TextInput from "./TextInput";
 
+function handleChange(event) {
+  console.log(event.target.value);
+}
+
 storiesOf("TextInput", module)
   .add("Primary", () => (
-    <TextInput type={"text"} placeholder={"Sign up here"} />
+    <TextInput
+      type={"text"}
+      placeholder={"Sign up here"}
+      onChange={handleChange}
+    />
   ))
   .add("Full Name", () => (
-    <TextInput label={"Full Name*"} type={"text"} placeholder={""} />
+    <TextInput
+      label={"Full Name*"}
+      type={"text"}
+      placeholder={""}
+      onChange={handleChange}
+    />
   ))
   .add("Email", () => (
-    <TextInput label={"Email*"} type={"email"} placeholder={""} />
+    <TextInput
+      label={"Email*"}
+      type={"email"}
+      placeholder={""}
+      onChange={handleChange}
+    />
   ))
   .add("Phone Number", () => (
-    <TextInput label={"Phone Number*"} type={"number"} placeholder={""} />
+    <TextInput
+      label={"Phone Number*"}
+      type={"number"}
+      placeholder={""}
+      onChange={handleChange}
+    />
   ))
   .add("Number of Guests", () => (
-    <TextInput label={"Number of Guests"} type={"number"} placeholder={""} />
+    <TextInput
+      label={"Number of Guests"}
+      type={"number"}
+      placeholder={""}
+      onChange={handleChange}
+    />
   ));
