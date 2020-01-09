353ece0bee9298475422b3589530aabd48bf6c2c
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-08 22:35:18 -0500

    the component looks the same. next step is re-usability

---
+++ client/src/components/SeasonCollectionImageCardComponent/seasoncollectionimg.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import SeasonCollectionImageCardComponent from './seasoncollectionimg';
+
+storiesOf('SeasonCollectionImageCardComponent', module)
+    .add('SeasonCollection', () => <SeasonCollectionImageCardComponent />)
