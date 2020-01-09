9f1d10ee0e117196aa1d65a279b83119793f1cd0
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-09 09:29:24 -0500

    added in bridalappt.stories.js

---
+++ client/src/components/Bridal-Appt./Bridal-Appt.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import BridalAppt from './Bridal-Appt'
+
+storiesOf('BridalAppt', module)
+    .add('BridalAppt', () => <BridalAppt />)
