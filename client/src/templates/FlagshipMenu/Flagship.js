6393dcd2551ecdea11674aef70ce154cdf13fa4a
Adam DeLessio <adamdelessio@gmail.com>
2020-01-09 14:29:30 -0500

    working on corrections

--- client/src/templates/FlagshipMenu/Flagship.js
+++ client/src/templates/FlagshipMenu/Flagship.js
@@ -5,36 +5,36 @@ import Icons from '../Icons/FAIcon'
 const Menu = (props) => {
 	return(
 		<div className='flagship-container'>
-			<header>
+			<header className='flagship-header'>
 				<Icons name={['fas', 'arrow-left']} size='xs' />
 				<h2 className='flagship-city'>{props.city}</h2>
 			</header>
 			<img />
 			<section>
-				<h3>Store Contact Info</h3>
+				<h3 className='store-contact-info'>Store Contact Info</h3>
 				<hr className='flagship-line'/>
 				<div className='flagship-info'>
 					<Icons name={['fas', 'map-marker-alt']} />
 					<h4 className='contact-info-title'>Address</h4>
-					<br/>
+
 					<p className='contact-info'>{props.address}</p>
 				</div>
 				<div className='flagship-info'>
 					<Icons name={['fas', 'phone-alt']} />
 					<h4 className='contact-info-title'>Phone</h4>
-					<br/>
+
 					<p className='contact-info'>{props.phone}</p>
 				</div>
 				<div className='flagship-info'>
 					<Icons name={['far', 'envelope']} />
 					<h4 className='contact-info-title'>Email</h4>
-					<br/>
+
 					<p className='contact-info'>{props.email}</p>
 				</div>
 				<div className='flagship-info'>
 					<Icons name={['fab', 'instagram']} />
 					<h4 className='contact-info-title'>Instagram</h4>
-					<br/>
+
 					<p className='contact-info'>{props.instagram}</p>
 				</div>
 			</section>
--- client/src/templates/FlagshipMenu/Flagship.stories.js
+++ client/src/templates/FlagshipMenu/Flagship.stories.js
@@ -10,10 +10,4 @@ storiesOf('Flagship Menu', module)
     							 phone='(443) 447-8929' 
     							 email='baltimore@moderntrousseau.com' 
     							 instagram='Need more inspo? Follow us at @moderntrousseaubaltimore'
-
-    							 // back-arrow={['fas', 'arrow-left']}
-    							 // location-icon={['fas', 'map-marker-alt']}
-    							 // phone-icon={['fas', 'phone-alt']}
-    							 // email-icon={['far', 'envelope']}
-    							 // instagram-icon={['fab', 'instagram']}
     						/>))
