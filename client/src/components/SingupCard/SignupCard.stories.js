39c02dd3180551a729bd50129722b584e9b2c547
Jose Casado <jcasado6@yahoo.com>
2020-01-09 14:46:45 -0500

    began component, waiting on two other components to be merged

---
+++ client/src/components/SingupCard/SignupCard.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import SignupCard from './SignupCard';
+
+storiesOf('SignupCard', module)
+    .add('Signup', () => <SignupCard />)
