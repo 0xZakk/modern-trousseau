0ad85945bc90a484501cd37e0cc0aad3be64a43c
Mamady Sillah <supurpisces310@msn.com>
2020-01-09 10:00:45 -0500

    updated designer card to correct branch

--- client/src/components/Details/Details.stories.js
+++
@@ -1,9 +0,0 @@
-
-    // import React from 'react';
-    
-    // import { storiesOf } from '@storybook/react';
-    
-    // import Details from './Details'
-    
-    // storiesOf('Details', module)
-    //     .add('Details', () => <Button>This is a Button</Button>)
