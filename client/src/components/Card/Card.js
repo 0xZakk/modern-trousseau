ae33784bb5e341a13c2f739a0f2cd95f5e02008e
Adam DeLessio <adamdelessio@gmail.com>
2020-01-09 10:08:21 -0500

    imports correct svg

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Card.css'
+import Icons from '../Icons/FAIcon'
+
+let types = ['heart', 'calendar', 'dress']
+let useIcon = null
 
 const Card = (props) => {
+	if (types.includes(props.type)) {
+		useIcon = (props.icon)
+	}
+
 	return(
 		<div className='card-container'>
-			<div className='card-icon'>{props.icon}</div>
+			<Icons name={useIcon} size='3x' />
 			<h4 className='card-title'>{props.title}</h4>
 			<p className='card-paragraph'>{props.paragraph}</p>
 		</div>
