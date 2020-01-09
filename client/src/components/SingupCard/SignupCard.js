1c7c63638cf9b6e1de623fffbecc3e1b2e24da95
Jose Casado <jcasado6@yahoo.com>
2020-01-09 17:04:49 -0500

    saving

--- client/src/components/SingupCard/SignupCard.js
+++ client/src/components/SingupCard/SignupCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./SignupCard.css";
+import TextInput from '../TextInputComponent/TextInput'
 
 const SignupCard = props => (
   <div className="signup">
     <div className="container">
-      Obsessed with a dress?<form placeholder='sign up for updates'></form><button className="small-pink">Submit</button>
+      <TextInput/>Obsessed with a dress?<button className="small-pink">Submit</button>
     </div>
   </div>
 );
