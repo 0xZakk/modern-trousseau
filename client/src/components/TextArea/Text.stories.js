5cc5b5fd3ad166a10d9d7bbc9096f75395ef52c1
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-08 19:53:26 -0500

    DatePicker } number 27

--- client/src/components/TextArea/Text.stories.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import Text from './Text';
-
-storiesOf('Text', module)
-    .add('Default', () => <Text pClassList='text-area-title' textAreaClassList='text-area'/>)
