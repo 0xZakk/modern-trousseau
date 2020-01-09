b2903adbd6370eb39a79eb1ad9a3b5d5e8a3e4be
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-09 18:18:10 -0500

    put season, year and images in props

--- client/src/components/SeasonCollectionImageCardComponent/SeasonCollectionImage.js
+++ client/src/components/SeasonCollectionImageCardComponent/SeasonCollectionImage.js
@@ -3,18 +3,24 @@ import "./SeasonCollectionImage.css";
 import imageFile1 from "../SeasonCollectionImageCardComponent/SeasonCollectionAssetDirectory/Image 1.svg";
 import imageFile2 from "../SeasonCollectionImageCardComponent/SeasonCollectionAssetDirectory/Image 2.svg";
 
-let season = "Fall";
-let year = "2020";
-
 const SeasonCollectionImageCardComponent = props => (
   <div>
     <div className="season-collection-square">
       <div className="season-collection-image-container">
-        <img className="season-collection-image" src={imageFile1} alt="img" />
-        <img className="season-collection-image" src={imageFile2} alt="img" />
+        <img
+          className="season-collection-image"
+          src={props.image1}
+          alt="image"
+        />
+        <img
+          className="season-collection-image"
+          src={props.image2}
+          alt="image"
+        />
       </div>
+
       <p className="season-collection-text">
-        {season} {year} Collection
+        {props.season} {props.year} Collection
       </p>
     </div>
   </div>
