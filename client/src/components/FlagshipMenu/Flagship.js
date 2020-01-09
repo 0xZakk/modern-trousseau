3b848376f7897b228bfcf910f0938a62cf52df67
Adam DeLessio <adamdelessio@gmail.com>
2020-01-09 15:18:45 -0500

    styling changes

--- client/src/components/FlagshipMenu/Flagship.js
+++ client/src/components/FlagshipMenu/Flagship.js
@@ -6,33 +6,33 @@ const Menu = (props) => {
 	return(
 		<div className='flagship-container'>
 			<header className='flagship-header'>
-				<Icons name={['fas', 'arrow-left']} size='xs' />
+				<Icons className='flagship-icon' name={['fas', 'arrow-left']} size='xs' />
 				<h2 className='flagship-city'>{props.city}</h2>
 			</header>
-			<img />
-			<section>
+			<img className='flagship-image' />
+			<section className='flagship-section'>
 				<h3 className='store-contact-info'>Store Contact Info</h3>
 				<hr className='flagship-line'/>
 				<div className='flagship-info'>
-					<Icons name={['fas', 'map-marker-alt']} />
+					<Icons className='flagship-icon' name={['fas', 'map-marker-alt']} />
 					<h4 className='contact-info-title'>Address</h4>
 
 					<p className='contact-info'>{props.address}</p>
 				</div>
 				<div className='flagship-info'>
-					<Icons name={['fas', 'phone-alt']} />
+					<Icons className='flagship-icon' name={['fas', 'phone-alt']} />
 					<h4 className='contact-info-title'>Phone</h4>
 
 					<p className='contact-info'>{props.phone}</p>
 				</div>
 				<div className='flagship-info'>
-					<Icons name={['far', 'envelope']} />
+					<Icons className='flagship-icon' name={['far', 'envelope']} />
 					<h4 className='contact-info-title'>Email</h4>
 
 					<p className='contact-info'>{props.email}</p>
 				</div>
 				<div className='flagship-info'>
-					<Icons name={['fab', 'instagram']} />
+					<Icons className='flagship-icon' name={['fab', 'instagram']} />
 					<h4 className='contact-info-title'>Instagram</h4>
 
 					<p className='contact-info'>{props.instagram}</p>
