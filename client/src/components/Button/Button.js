1cd35a3b44205cb357ebb21617c7de4b63a98cef
Mamady Sillah <supurpisces310@msn.com>
2020-01-09 08:55:34 -0500

    used a more descriptive name 'showMoreInformation' on the on click function to give more clairty on what it does

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -5,9 +5,12 @@ import './Button.css'
 
 const Button = (props) => {
 let classList = "button";
-let types = ['primary','details']
+let types = ['primary', 'details']
+if (types.includes(props.type)) {
+    classList += ` button-${props.type}`
+}
  
-return <button classNme={classList} onClick={props.onClick}>{props.label}</button>
+return <button className={classList} onClick={props.onClick}>{props.label}</button>
 }
 
 export default Button;
--- client/src/components/Button/Button.stories.js
+++ client/src/components/Button/Button.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import './Button.css'
 import Button from './Button';
 
-function handleClick(e) {
+function showMoreInformation(e) {
     e.preventDefault();
     alert("Dress Details")
 }
@@ -11,4 +11,4 @@ function handleClick(e) {
 
 storiesOf('Button', module)
     .add('Primary', () => <Button label="This is a Button" type="primary"/>)
-    .add('Details', () => <Button label="+" type="details" onClick={handleClick} />)
+    .add('Details', () => <Button label="+" type="details" onClick={showMoreInformation} />)
