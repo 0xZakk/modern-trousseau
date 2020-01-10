008b2300c21d0162ac000284d75b05279f150aac
Kelly M Collins <90kcol@gmail.com>
2020-01-10 14:15:53 -0500

    added -style to desktop and mobile classNames

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -6,21 +6,21 @@ import logo from './Logo/Logo.png';
 const NavBar = () => (
     <div>
         <nav className='nav-bar'>
-            <div className='mobile'>
+            <div className='mobile-style'>
                 {/* Replace with burger component when complete */}
                 <Icons name={['fas', 'bars']}/>
             </div>
-            <div className='mobile'>
+            <div className='mobile-style'>
                 {/* Replace with logo component */}
                 <img src ={logo} height='10' width ='100' alt = 'Modern trousseau logo' />
             </div>
             <div className='nav-icons'>
                 <Icons name={['fas', 'search']}/>
             </div>
-            <div className='desktop'>
+            <div className='desktop-style'>
                 <Icons name={['fas', 'map-marker-alt']}/>
             </div>
-            <div className='desktop'>
+            <div className='desktop-style'>
                 <Icons name={['far', 'user']}/>
             </div>
         </nav>
