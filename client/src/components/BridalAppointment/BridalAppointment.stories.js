6512694894236cbd8d58b2ea11b89daed6aa78eb
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-10 13:24:25 -0500

    finished styling icons

--- client/src/components/BridalAppointment/BridalAppointment.stories.js
+++ client/src/components/BridalAppointment/BridalAppointment.stories.js
@@ -23,14 +23,14 @@ storiesOf("Information Banner", module)
         <h4>Book Appointment</h4>
         <div className="icon-container">
           <p className="icon-details">
-            <Icons name={["fas", "calendar-day"]} size="xs" /> Thursday,
+            <Icons name={["fas", "calendar-day"]} size="sm" />&nbsp;&nbsp;Thursday,
             November 7, 2019
           </p>
           <p className="icon-details">
-            <Icons name={["fas", "calendar-day"]} size="xs" /> Washington, DC
+            <Icons name={["fas", "map-marker-alt"]} size="sm" />&nbsp;&nbsp;Washington, DC
           </p>
           <p className="icon-details">
-            <Icons name={["fas", "calendar-day"]} size="xs" /> 9:00-10:00am
+            <Icons name={['far', 'clock']} size="sm" />&nbsp;&nbsp;9:00-10:00am
           </p>
         </div>
       </div>
