de2d968fea24893d62d313b7eab1696b573f913d
Kelly M Collins <90kcol@gmail.com>
2020-01-10 11:39:47 -0500

    Merge

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -5,15 +5,23 @@ import logo from './Logo/Logo.png';
 
 const NavBar = () => (
     <div>
-        <nav className='navBar'>
-            <div>
+        <nav className='nav-bar'>
+            <div className='mobile'>
+                {/* Replace with burger component when complete */}
                 <Icons name={['fas', 'bars']}/>
             </div>
-            <div>
+            <div className='mobile'>
+                {/* Replace with logo component */}
                 <img src ={logo} height='10' width ='100' alt = 'Modern trousseau logo' />
             </div>
-            <div>
+            <div className='nav-icons'>
                 <Icons name={['fas', 'search']}/>
+            </div>
+            <div className='desktop'>
+                <Icons name={['fas', 'map-marker-alt']}/>
+            </div>
+            <div className='desktop'>
+                <Icons name={['far', 'user']}/>
             </div>
         </nav>
     </div>
