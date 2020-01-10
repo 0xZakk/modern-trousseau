a4b9a3e6d5d90a58d35db712527499dfea87381f
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-10 10:48:25 -0500

    Adding all ancillary files.

--- client/src/components/BrideInfo/BrideInfo.js
+++ client/src/components/BrideInfo/BrideInfo.js
@@ -1,29 +1,37 @@
 import React from "react";
-import DatePicker from '../DatePicker/DatePicker'
-import TextInput from '../TextInputComponent/TextInput'
+import DatePicker from "../DatePicker/DatePicker";
+import TextInput from "../TextInputComponent/TextInput";
 
-const BrideInfo = (props) =>
-
-<div>
-   <TextInput
+const BrideInfo = props => (
+  <div>
+    <TextInput
       label={"Full Name*"}
       type={"text"}
       placeholder={""}
       onChange={props.handleClick}
-      />
-   <TextInput
+    />
+    <TextInput
       label={"Phone Number*"}
       type={"number"}
       placeholder={""}
       onChange={props.handleClick}
-      />
+    />
     <TextInput
       label={"Email*"}
       type={"email"}
       placeholder={""}
       onChange={props.handleClick}
-      />
-<DatePicker label={"Requested Date*"} name={"Requested Date"} value={"November 11"} min={"February 29, 2020"} step={"1"} max={"February 28, 2022"} onChange={props.handleChange}/>
-</div>
+    />
+    <DatePicker
+      label={"Requested Date*"}
+      name={"Requested Date"}
+      value={"November 11"}
+      min={"February 29, 2020"}
+      step={"1"}
+      max={"February 28, 2022"}
+      onChange={props.handleChange}
+    />
+  </div>
+);
 
-export default BrideInfo
+export default BrideInfo;
