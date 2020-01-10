dba8faffce070a294592c6a89336c13e5c281d7a
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-01-10 11:47:18 -0500

    quick commit before merging

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,6 +1,6 @@
-import { configure, addDecorator } from '@storybook/react';
-import StoryRouter from 'storybook-react-router';
+import { configure, addDecorator } from "@storybook/react";
+import StoryRouter from "storybook-react-router";
 
 addDecorator(StoryRouter());
 
-configure(require.context('../src/components', true, /\.stories\.js$/), module)
+configure(require.context("../src/components", true, /\.stories\.js$/), module);
