e46d15064f21e9e0d959ffa2223d254664617a02
Jose Casado <jcasado6@yahoo.com>
2020-01-10 12:19:44 -0500

    saving

--- client/src/components/SingupCard/SignupCard.js
+++ client/src/components/SingupCard/SignupCard.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./SignupCard.css";
+import TextInput from "../TextInputComponent/TextInput";
+
+function handleChange(event) {
+  console.log(event.target.value);
+}
 
 const SignupCard = props => (
-  <div className='signup'>
-    Obsessed with a dress?
+  <div className="container">
+    <div className="text">Obsessed with a dress?
+    <div className="signup">
+      {" "}
+      <TextInput
+        type={"text"}
+        placeholder={"Sign up here"}
+        onChange={handleChange}
+      />
+      </div>
+    </div>
   </div>
 );
 
