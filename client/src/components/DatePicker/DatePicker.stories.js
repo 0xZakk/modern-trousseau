039d28a6adb20b78c6f6e89e900b220a10083df1
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-10 09:28:00 -0500

    commited after Karadi fixed errors with running storybook in this branch

--- client/src/components/DatePicker/DatePicker.stories.js
+++ client/src/components/DatePicker/DatePicker.stories.js
@@ -9,4 +9,4 @@
 // }
 
 // storiesOf('DatePicker', module)
-//     .add('DatePicker', () => <DatePicker label={"Requested Date*"} name={"Requested Date"} value={"November 11"} min={"February 29, 2020"} step={"1"} max={"February 28, 2022") onChange={handleChange}/>)
+//     .add('DatePicker', () => <DatePicker label={"Requested Date*"} name={"Requested Date"} value={"November 11"} min={"February 29, 2020"} step={"1"} max={"February 28, 2022"} onChange={handleChange}/>)
