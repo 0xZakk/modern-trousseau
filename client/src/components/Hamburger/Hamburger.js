887ad5e62afdb63ce7849feaf33e5c8100dffe67
AlexGarcia <afgarcia666@gmail.com>
2020-01-11 15:32:32 -0500

    refactored my code and put all of my react hooks function and logic inside my component file. Can not get logi to pass down keeps coming up undefined. deleted commentedout code

--- client/src/components/Hamburger/Hamburger.js
+++ client/src/components/Hamburger/Hamburger.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -6,45 +6,20 @@ import './Hamburger.css';
 
 library.add(faBars, faTimes)
 
-// class Hamburger extends Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             open: false,
-//             iconList: "bars",
-//             typeName: "hamburgerBars"
-//         }
-//         this.openMenu = this.openMenu.bind(this)
-//     }
-//     openMenu() {
-//         this.setState({
-//         ...this.state,
-//         iconList: "times",
-//         typeName: "hamburgerTimes"})
-//         console.log(this.state.iconList + " " + this.state.class);
-//         }
-    
-//     render() {
-//     return (
-//     <a className="hamburger-menu" onClick={this.openMenu} >
-//         <FontAwesomeIcon
-//         icon={this.state.iconList} 
-//         className={this.state.typeName} 
-//         />
-//     </a>
-//     )
-//     }
-// };
 
-
-const Hamburger = (props) => 
-
-    <a className="hamburger-menu" onClick={props.onClick} >
-        <FontAwesomeIcon
-        icon={props.icon} 
-        className={props.className} 
-        />
-    </a>
+const Hamburger = (props) => {
+    const {icons, setIcons} = useState("bars");
+    console.log(icons)
+    const switchIcon = () => {
+        setIcons("times");
+    }
+    return <a className="hamburger-menu" onClick={props.onClick} >
+            <FontAwesomeIcon
+            onClick={switchIcon}
+            className="HamburgerIcon"
+            icon={icons}/>
+            </a>
+}
 
 
 export default Hamburger;
--- client/src/components/Hamburger/Hamburger.stories.js
+++ client/src/components/Hamburger/Hamburger.stories.js
@@ -1,33 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react'; 
 import Hamburger from './Hamburger';
 
-let burger = "hamburgerBars";
-let icons = "bars";
 
-function switchMenu() {
-    // const [burger, setBurger] = useState("hamburgerTimes");
-    const [icons, setIcons] = useState("times");
-}
-
-function switchBack() {
-    // const [burger, setBurger] = useState("hamburgerBars");
-    const [icons, setIcons] = useState("bars");
-}
-
-function handleClick() {
-    console.log(burger)
-    if(burger == "hambugerBars" && icons == "bars"){
-    return(switchMenu)
-    }
-    else{
-    return(switchBack)
-    
-    }
-    
-}
 
 storiesOf('Hamburger', module)
     .add('Hamburger', () => 
-    <Hamburger className={burger} icon={icons} onClick={handleClick}></Hamburger>)
+    <Hamburger />)
 
