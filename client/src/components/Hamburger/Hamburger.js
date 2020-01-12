c6d60cce1a5d5e08a8ddd3b82779066adf3f39dd
AlexGarcia <afgarcia666@gmail.com>
2020-01-12 09:46:45 -0500

    put react hooks back in stories.js and now the code has more bugs

--- client/src/components/Hamburger/Hamburger.js
+++ client/src/components/Hamburger/Hamburger.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -6,18 +6,16 @@ import './Hamburger.css';
 
 library.add(faBars, faTimes)
 
-
 const Hamburger = (props) => {
-    const {icons, setIcons} = useState("bars");
-    console.log(icons)
-    const switchIcon = () => {
-        setIcons("times");
-    }
+    // const {icons, setIcons} = useState("bars");
+    // console.log(icons)
+    // const switchIcon = () => {
+    //     setIcons("times");
+    // }
     return <a className="hamburger-menu" onClick={props.onClick} >
             <FontAwesomeIcon
-            onClick={switchIcon}
             className="HamburgerIcon"
-            icon={icons}/>
+            icon={props.icon}/>
             </a>
 }
 
--- client/src/components/Hamburger/Hamburger.stories.js
+++ client/src/components/Hamburger/Hamburger.stories.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react'; 
 import Hamburger from './Hamburger';
 
+let open = false;
+let icons = ""
+
+const iconType = () => {
+    const {icons, setIcons} = useState("bars")
+}
+
+const switchIcon = () => {
+    if (open === true && icons === "bars"){
+        setIcons("times")
+    }else {
+        setIcons("bars")
+    }
+}
 
 
 storiesOf('Hamburger', module)
     .add('Hamburger', () => 
-    <Hamburger />)
+    <Hamburger 
+    icon={icons}
+    open={open} 
+    onClick={switchIcon}/>)
 
