5e2e4a8ac8a6d3eb2d06116c1ee8ed90ff35bb4e
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-04 14:29:09 -0500

    working on getting the individual carousels to render

--- client/src/components/DisplayComponent/DisplayComponent.js
+++ client/src/components/DisplayComponent/DisplayComponent.js
@@ -3,72 +3,16 @@ import ReactDOM from "react-dom";
 import Slider from "../Slider/Slider"
 import "./DisplayComponent.css";
 
-import imageFile1 from "../../Images/Fall-2020-1.png";
-import imageFile2 from "../../Images/Fall-2020-2.png";
-import imageFile3 from "../../Images/Spring-2020-1.jpeg";
-import imageFile4 from "../../Images/Spring-2020-2.jpeg";
-import imageFile5 from "../../Images/Trending-Now-1.jpeg";
-import imageFile6 from "../../Images/Trending-Now-2.jpeg";
-import imageFile7 from "../../Images/Classics-1.jpeg";
-import imageFile8 from "../../Images/Classics-2.jpeg";
-import { Slide } from "pure-react-carousel";
-
-const slides = {
-  properties: [
-    {
-      title: "Fall 2020 Collection",
-      imga: imageFile1,
-      imgb: imageFile2,
-      labela: "LUCA",
-      labelb: "VIDA",
-      index: 0
-    },
-    {
-      title: "Spring 2020 Collection",
-      imga: imageFile3,
-      imgb: imageFile4,
-      labela: "AlWAYS",
-      labelb: "BELIZE",
-      index: 1
-    },
-    {
-      title: "Trending Now",
-      imga: imageFile5,
-      imgb: imageFile6,
-      labela: "ALICE",
-      labelb: "EVELYN",
-      index: 2
-    },
-    {
-      title: "Classics",
-      imga: imageFile7,
-      imgb: imageFile8,
-      labela: "AINSLEY",
-      labelb: "ANNIE",
-      index: 3
-    }
-  ]
-};
-
-
 class DisplayComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // properties: slides.properties,
-      // property: slides.properties[0]
+      properties: this.props.slideData,
+      property: this.props.slideData[0]
     };
   }
   
-  // nextProperty = () => {
-  //   const newIndex = this.state.property.index + 1;
-  //   this.setState({
-  //     property: slides.properties[newIndex]
-  //   });
-  // };
-  
   render() {
-    // const { properties, property } = this.state;
     return (
       <div>
         <h3 className="display-component-title">GOWNS</h3>
@@ -80,8 +24,6 @@ class DisplayComponent extends React.Component {
     );
   }
 }
-
-// export default Slider;
 
 export default DisplayComponent;
 
--- client/src/components/DisplayComponent/DisplayComponent.stories.js
+++ client/src/components/DisplayComponent/DisplayComponent.stories.js
@@ -1,8 +1,53 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 
+import Slider from "../Slider/Slider.js";
 import DisplayComponent from "../DisplayComponent/DisplayComponent";
 
+import imageFile1 from "../../Images/Fall-2020-1.png";
+import imageFile2 from "../../Images/Fall-2020-2.png";
+import imageFile3 from "../../Images/Spring-2020-1.jpeg";
+import imageFile4 from "../../Images/Spring-2020-2.jpeg";
+import imageFile5 from "../../Images/Trending-Now-1.jpeg";
+import imageFile6 from "../../Images/Trending-Now-2.jpeg";
+import imageFile7 from "../../Images/Classics-1.jpeg";
+import imageFile8 from "../../Images/Classics-2.jpeg";
+
+const slideData = [
+  {
+    title: "Fall 2020 Collection",
+    imga: imageFile1,
+    imgb: imageFile2,
+    labela: "LUCA",
+    labelb: "VIDA",
+    index: 0
+  },
+  {
+    title: "Fall 2020 Collection",
+    imga: imageFile3,
+    imgb: imageFile4,
+    labela: "AlWAYS",
+    labelb: "BELIZE",
+    index: 1
+  },
+  {
+    title: "Fall 2020 Collection",
+    imga: imageFile5,
+    imgb: imageFile6,
+    labela: "ALICE",
+    labelb: "EVELYN",
+    index: 2
+  },
+  {
+    title: "Fall 2020 Collection",
+    imga: imageFile7,
+    imgb: imageFile8,
+    labela: "AINSLEY",
+    labelb: "ANNIE",
+    index: 3
+  }
+];
+
 storiesOf("Gown-Display-Component", module).add("Gown Display Component", () => (
-    <DisplayComponent type="gowns-stack" />
+    <DisplayComponent slideData={slideData} type="gowns-stack" />
   ));
