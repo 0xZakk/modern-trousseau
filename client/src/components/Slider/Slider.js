1de350adb7b6f837d7f54da2b1403d6d150a6e30
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-04 14:20:59 -0500

    need css

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -32,32 +32,27 @@ class Slider extends React.Component {
 
   prevProperty = () => {
     if (this.state.property.index >= 0) {
-      console.log(this.state.property.index);
       const newIndex = this.state.property.index - 1;
       this.setState({
         property: this.props.slideData[newIndex]
       });
-    } 
+    }
     if (this.state.property.index == 0) {
-          console.log(this.state.property.index)
-          const newIndex = this.state.property.index - 1;
-          this.setState({
-          property: this.props.slideData[newIndex]
-    });
-  }
-};
-
+      const newIndex = this.state.property.index - 1;
+      this.setState({
+        property: this.props.slideData[newIndex]
+      });
+    }
+  };
 
   nextProperty = () => {
     if (this.state.property.index >= 0) {
-      console.log(this.state.property.index);
       const newIndex = this.state.property.index + 1;
       this.setState({
         property: this.props.slideData[newIndex]
       });
     }
     if (this.state.property.index == this.props.slideData.length - 1) {
-      console.log(this.state.property.index);
       const newIndex = 0;
       this.setState({
         property: this.props.slideData[newIndex]
