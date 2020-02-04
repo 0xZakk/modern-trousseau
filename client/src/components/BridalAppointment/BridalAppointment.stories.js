e8ff695863b0cfe10ef45c0a18de28f5d8790755
Jenny_Ripper <jennifer.ripper@gmail.com>
2020-02-04 15:22:58 -0500

    started confirmation box

--- client/src/components/BridalAppointment/BridalAppointment.stories.js
+++ client/src/components/BridalAppointment/BridalAppointment.stories.js
@@ -4,20 +4,9 @@ import { storiesOf } from "@storybook/react";
 import InformationBanner from "./BridalAppointment";
 import Icons from "../Icons/FAIcon";
 import Tassel from "./Tassel.svg";
+import Button from "../Button/Button";
 
 storiesOf("Information Banner", module)
-  .add("Bridal Appointment Default", () => (
-    <InformationBanner>
-      <h4 className="mobile-default-info">Book Appointments</h4>
-      <p className="main-text">
-        Modern Trousseau appointments are truly about you, the bride. Our
-        Flagship stores offer private appointment for you, your family and
-        friends, to create your dream wedding gown. With every Modern Trousseau
-        gown made in Woodbridge, Connecticut, be prepared for fun, bubbles, and
-        that magic moment where you find your dream dress.
-      </p>
-    </InformationBanner>
-  ))
   .add("Book Appointment", () => (
     <InformationBanner>
       <div className="info-view">
@@ -39,23 +28,62 @@ storiesOf("Information Banner", module)
       </div>
     </InformationBanner>
   ))
-  .add("Bridal Appointment Info Desktop", () => (
+  .add("Bridal Appointment Info", () => (
     <InformationBanner>
-      <h4 className="bridal-appt-desktop">Book Appointments</h4>
+      <h4 className="bridal-appt-h4">Book Appointments</h4>
       <img src={Tassel} className="dress-icon" />
+      <p className="main-text">
+        Modern Trousseau appointments are truly about you, the bride. Our
+        Flagship stores offer private appointment for you, your family and
+        friends, to create your dream wedding gown. With every Modern Trousseau
+        gown made in Woodbridge, Connecticut, be prepared for fun, bubbles, and
+        that magic moment where you find your dream dress.
+      </p>
       <div className="width-max">
-        <p className="main-text">
-          Modern Trousseau appointments are truly about you, the bride. Our
-          Flagship stores offer private appointment for you, your family and
-          friends, to create your dream wedding gown. With every Modern
-          Trousseau gown made in Woodbridge, Connecticut, be prepared for fun,
-          bubbles, and that magic moment where you find your dream dress.
-        </p>
         <br />
         <p className="main-text">
           A Modern Trousseau team member will follow-up within 24 hours to
           confirm your appointment date and time. See you in the Flagship soon!
         </p>
+      </div>
+    </InformationBanner>
+  ))
+  .add("Bridal Appointment Confirmation Info", () => (
+    <InformationBanner>
+      <div className="times-close">
+        <Icons name={["fas", "times"]} />
+      </div>
+      <div className="appt-confirmation">
+        <div className="appt-img">
+          <img src={Tassel} className="appt-icon" />
+        </div>
+        <div className="confirmation-info">
+          <div className="appt-name">Thank You, Melanie!</div>
+          <span className="appt-name-sub">
+            We’ll get back to you within 24 hours via email to confirm your
+            appointment date & time. See you soon!
+          </span>
+          <div className="appt-data">
+            <p className="appt-details">
+              <Icons name={["fas", "calendar-day"]} size="sm" />
+              &nbsp;&nbsp;Thursday, November 7, 2019
+            </p>
+            <p className="appt-details">
+              <Icons name={["fas", "map-marker-alt"]} size="sm" />
+              &nbsp;&nbsp;Washington, DC
+            </p>
+            <p className="appt-details">
+              <Icons name={["far", "clock"]} size="sm" />
+              &nbsp;&nbsp;9:00-10:00am
+            </p>
+          </div>
+          <div className="appt-btn">
+            <Button label="Browse Dresses" type="red" large />
+            </div>
+            <div className="appt-btn-2">
+            <Button label="Watch Our Video" type="red" large />
+          </div>
+        </div>
       </div>
     </InformationBanner>
   ));
