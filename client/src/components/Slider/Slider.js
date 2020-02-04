e419c7f384a9fa4f0aaabec9e2afd9bbd490d50b
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-04 13:37:23 -0500

    fixed some props issues

--- client/src/components/Slider/Slider.js
+++ client/src/components/Slider/Slider.js
@@ -1,52 +1,7 @@
-import imageFile1 from "../../Images/Fall-2020-1.png";
-import imageFile2 from "../../Images/Fall-2020-2.png";
-import imageFile3 from "../../Images/Spring-2020-1.jpeg";
-import imageFile4 from "../../Images/Spring-2020-2.jpeg";
-import imageFile5 from "../../Images/Trending-Now-1.jpeg";
-import imageFile6 from "../../Images/Trending-Now-2.jpeg";
-import imageFile7 from "../../Images/Classics-1.jpeg";
-import imageFile8 from "../../Images/Classics-2.jpeg";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./Slider.css";
 
-const slides = {
-  properties: [
-    {
-      title: "Fall 2020 Collection",
-      imga: imageFile1,
-      imgb: imageFile2,
-      labela: "LUCA",
-      labelb: "VIDA",
-      index: 0
-    },
-    {
-      title: "Spring 2020 Collection",
-      imga: imageFile3,
-      imgb: imageFile4,
-      labela: "AlWAYS",
-      labelb: "BELIZE",
-      index: 1
-    },
-    {
-      title: "Trending Now",
-      imga: imageFile5,
-      imgb: imageFile6,
-      labela: "ALICE",
-      labelb: "EVELYN",
-      index: 2
-    },
-    {
-      title: "Classics",
-      imga: imageFile7,
-      imgb: imageFile8,
-      labela: "AINSLEY",
-      labelb: "ANNIE",
-      index: 3
-    }
-  ]
-};
 
 const Slide = ({ property }) => {
   const { title, imga, imgb, labela, labelb, index } = property;
@@ -71,8 +26,8 @@ class Slider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      properties: slides.properties,
-      property: slides.properties[0]
+      properties: this.props.slideData,
+      property: this.props.slideData[0]
     };
   }
 
@@ -82,27 +37,33 @@ class Slider extends React.Component {
           console.log(this.state.property.index)
           const newIndex = this.state.property.index - 1;
           this.setState({
-      property: slides.properties[newIndex]
-    })} if (this.state.property.index == 0) {
+        property: this.props.slideData[newIndex]
+      })
+    } 
+    if (this.state.property.index == 0) {
           console.log(this.state.property.index)
-          const newIndex = slides.properties.length - 1;
+          const newIndex = this.state.property.index - 1;
           this.setState({
-          property: slides.properties[newIndex]
-    })}
-  };
+          property: this.props.slideData[newIndex]
+    });
+  }
+};
 
   nextProperty = () => {
     if (this.state.property.index >= 0) {
       console.log(this.state.property.index)
       const newIndex = this.state.property.index + 1;
       this.setState({
-      property: slides.properties[newIndex]
-})} if (this.state.property.index == slides.properties.length - 1) {
+      property: this.props.slideData[newIndex]
+      })
+    } 
+    if (this.state.property.index == this.props.slideData.length - 1) {
       console.log(this.state.property.index)
       const newIndex = 0;
       this.setState({
-      property: slides.properties[newIndex]
-})}
+      property: this.props.slideData[newIndex]
+    });
+  }
 };
 
   render() {
--- client/src/components/Slider/Slider.stories.js
+++ client/src/components/Slider/Slider.stories.js
@@ -3,6 +3,55 @@ import { storiesOf } from "@storybook/react";
 
 import Slider from "./Slider";
 
-storiesOf("Slider", module).add("Gown Display Static", () => (
-  <Slider type="gowns" />
+import imageFile1 from "../../Images/Fall-2020-1.png";
+import imageFile2 from "../../Images/Fall-2020-2.png";
+import imageFile3 from "../../Images/Spring-2020-1.jpeg";
+import imageFile4 from "../../Images/Spring-2020-2.jpeg";
+import imageFile5 from "../../Images/Trending-Now-1.jpeg";
+import imageFile6 from "../../Images/Trending-Now-2.jpeg";
+import imageFile7 from "../../Images/Classics-1.jpeg";
+import imageFile8 from "../../Images/Classics-2.jpeg";
+
+const slideData = [
+  {
+    title: "Fall 2020 Collection",
+    imga: imageFile1,
+    imgb: imageFile2,
+    labela: "LUCA",
+    labelb: "VIDA",
+    index: 0
+  },
+  {
+    title: "Spring 2020 Collection",
+    imga: imageFile3,
+    imgb: imageFile4,
+    labela: "AlWAYS",
+    labelb: "BELIZE",
+    index: 1
+  },
+  {
+    title: "Trending Now",
+    imga: imageFile5,
+    imgb: imageFile6,
+    labela: "ALICE",
+    labelb: "EVELYN",
+    index: 2
+  },
+  {
+    title: "Classics",
+    imga: imageFile7,
+    imgb: imageFile8,
+    labela: "AINSLEY",
+    labelb: "ANNIE",
+    index: 3
+  }
+];
+
+storiesOf("Slider", module).add("Gown Display Slider", () => (
+  <Slider 
+    slideData={slideData}
+    title="Classics">
+
+  </Slider>
 ));
+
