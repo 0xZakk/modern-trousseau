39ea365c50b43e24858249195d0036f6e595a0c3
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-04 22:01:54 -0500

    began gown display component build.  solved state problem so text and images are rendering.  need link next to title at the top of the page and footer below the last slider

--- client/src/components/DisplayComponent/DisplayComponent.js
+++ client/src/components/DisplayComponent/DisplayComponent.js
@@ -15,94 +15,21 @@ class DisplayComponent extends React.Component {
   }
   
   render() {
+    console.log(this.state)
     return (
       <div>
         <h3 className="display-component-title">GOWNS</h3>
-        {/* <Slider/> */}
-        {/* <SignupCard/> */}
-        {/* <Slider className="slider-2"></Slider>
-        <Slider className="slider-3"></Slider>
-        <Slider className="slider-4"></Slider> */}
+        <p className='collection-title'>Collection 1</p>
+        <Slider slideData={this.state.properties} className=' slideshow collection-1'/>
+        <p className='collection-title'>Collection 1</p>
+        <Slider slideData={this.state.properties} className=' slideshow collection-2'/>
+        <p className='collection-title'>Collection 1</p>
+        <Slider slideData={this.state.properties} className=' slideshow collection-3'/>
+        <p className='collection-title'>Collection 1</p>
+        <Slider slideData={this.state.properties} className=' slideshow collection-4'/>
       </div>
     );
   }
 }
 
 export default DisplayComponent;
-
-
-
-
-
-
-
-
-
-// const Card = ({ property }) => {
-//   const { title, imga, imgb, labela, labelb, index } = property;
-//   return (
-//     <div id={`slide-${index}`} className="display-component-slide">
-//       <p className="display-component-collection-title card-part">{`${title}`}</p>
-//       <div className="display-component-container">
-//         <div className="image-1">
-//           <img src={imga} className="display-component-pic"/>
-//           <p className="display-component-text">{`${labela}`}</p>
-//         </div>
-//         <div className="image-2">
-//           <img src={imgb} className="display-component-pic"/>
-//           <p className="display-component-text">{`${labelb}`}</p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-
-{/* <div className="display-component-card display-component-card-1">
-  <div className="display-component-slide">
-    <Card property={property} />
-  </div>
-  <button
-    className="display-component-button"
-    onClick={() => this.nextProperty()}
-    disabled={property.index === slides.properties.length - 1}
-  >
-    &#10095;
-  </button>
-</div>
-<div className="display-component-card display-component-card-2">
-  <div className="display-component-slide">
-    <Card property={property} />
-  </div>
-  <button
-    className="display-component-button"
-    onClick={() => this.nextProperty()}
-    disabled={property.index === slides.properties.length - 1}
-  >
-    &#10095;
-  </button>
-</div> 
-<div className="display-component-card display-component-card-2">
-  <div className="display-component-slide">
-    <Card property={property} />
-  </div>
-  <button
-    className="display-component-button"
-    onClick={() => this.nextProperty()}
-    disabled={property.index === slides.properties.length - 1}
-  >
-    &#10095;
-  </button>
-</div> 
-<div className="display-component-card display-component-card-2">
-  <div className="display-component-slide">
-    <Card property={property} />
-  </div>
-  <button
-    className="display-component-button"
-    onClick={() => this.nextProperty()}
-    disabled={property.index === slides.properties.length - 1}
-  >
-    &#10095;
-  </button>
-</div>  */}
